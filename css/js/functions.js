b3bfe2f2ad9b02902b10267b7aa9bd7e716651a8
Goldfinch <pavepy@yandex.ru>
2013-10-31 17:45:20 +0400

    Переделал практически все файлы. Все кнопки запихнул в mixins-buttons Все миксины, заменяющие вендорные префиксы, кинул mixins-attributes Все миксины, содержащие разные наборы правил, в mixins В файлах mixins*.less все миксины имеют скобочки и не являются обычными классами — они не будут применяться сразу к дом--дереву, и не будут компилиться в css, если их не вызвали
    Файл style-base содержит классы. Часть перенес в mixins*.less и добавил скобочки. Все что в style-base — компилится в css. Там стили для шапки, футера, типографика. Его надо будет активно поддерживать. Добавил ASCII-арт
    
    Файл vars убрал вообще. Все в mixins*.less
    
    Стили bxslider'a переписал под less (со вложенностью). Называется slider.less
    
    в project.less стоит писать вначале все цвета (берем из брендбука_), которые мы переназначаем. Потому что по дефолту в style.less стоят стандартные цвета.

---
+++ css/js/functions.js
@@ -0,0 +1,102 @@
+//fitText if needed
+//jQuery(".phones b").fitText();
+
+$(document).ready(function(){
+// Главная страница
+document.addEventListener("touchstart", function(){}, true);
+
+$('#menuBody h3').append('<span>');
+$('#menuBody h3 span').bind('click',function() {
+  $(this).toggleClass('active');
+  $(this).parent().next().slideToggle();
+  });
+
+$('a').attr('href','#');
+$('.picAndTextBlock br, .b-banner br, .b-catalog__bals br').after('<span class="space"> </span>');
+
+
+});
+
+
+
+
+
+// ----------------------------
+// Показать верхнее меню
+// ----------------------------
+
+$('header .btn a').bind('click',function() {
+  var thisLink = $(this);
+  var openBlockId = thisLink.parent().attr('id');
+  var openBlockId = openBlockId+'Body';
+  var openBlock = $('.hidden-block#'+openBlockId);
+  openBlock.slideToggle();
+  thisLink.toggleClass('active');
+  $('.hidden-block').not(openBlock).slideUp();
+  $('header .btn a').not(thisLink).removeClass('active');
+});
+
+
+// ----------------------------
+// Переключение видов
+// ----------------------------
+$('.btn-group > .inside > .btn').bind('click',function() {
+  var t = $(this);
+  var p = t.parents('.multi-view');
+  p.find('.btn').removeClass('active');
+  p.find('.view').hide();
+  t.addClass('active');
+  p.find('.view:eq('+ t.index() +')').fadeIn(300);
+});
+$('.btn-group > .inside > .btn:first-child').addClass('active');
+$('.view-group > .view:first-child').show();
+
+
+// ----------------------------
+// Плавная прокрутка
+// ----------------------------
+ function filterPath(string) {
+  return string
+    .replace(/^\//,'')
+    .replace(/(index|default).[a-zA-Z]{3,4}$/,'')
+    .replace(/\/$/,'');
+  }
+  var locationPath = filterPath(location.pathname);
+  var scrollElem = scrollableElement('html', 'body');
+ 
+  $('a[href*=#]').each(function() {
+    var thisPath = filterPath(this.pathname) || locationPath;
+    if (  locationPath == thisPath
+    && (location.hostname == this.hostname || !this.hostname)
+    && this.hash.replace(/#/,'') ) {
+      var $target = $(this.hash), target = this.hash;
+      if (target) {
+        var targetOffset = $target.offset().top;
+        $(this).click(function(event) {
+          event.preventDefault();
+          $(scrollElem).animate({scrollTop: targetOffset}, 400, function() {
+            location.hash = target;
+          });
+        });
+      }
+    }
+  });
+ 
+  // use the first element that is "scrollable"
+  function scrollableElement(els) {
+    for (var i = 0, argLength = arguments.length; i <argLength; i++) {
+      var el = arguments[i],
+          $scrollElement = $(el);
+      if ($scrollElement.scrollTop()> 0) {
+        return el;
+      } else {
+        $scrollElement.scrollTop(1);
+        var isScrollable = $scrollElement.scrollTop()> 0;
+        $scrollElement.scrollTop(0);
+        if (isScrollable) {
+          return el;
+        }
+      }
+    }
+    return [];
+  }
